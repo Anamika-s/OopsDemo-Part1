8b2e7ebd223fd1b5fa7f478063d93798cc82caf3
Anamika Sawhney <anamika.sawhney22@gmail.com>
2024-06-28 11:03:49 +0530

    Add project files.

---
+++ OopsDemo/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+
+namespace OopsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x;
+            //Student student;
+            //student = new Student();
+            //Student student = new Student();
+            //// new keyword
+            //// 1. It allocates memory on heap
+            //// 2. it calls constructor (default) to initialize member variables
+            //student.GetDetails();
+            //student.DisplayDetails();
+
+            //Console.WriteLine("studnet 1");
+            //// Take 2 students
+            //Student student1 = new Student();
+            //student1 .GetDetails();
+            //student1.DisplayDetails();
+
+            //Console.WriteLine("student 2");
+            //Student student2 = new Student();
+            //student2.GetDetails();
+            //student2.DisplayDetails();
+
+            ////Take details of 10 Students
+            //// array of objects
+
+            //Student[] students = new Student[10];
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    students[i] = new Student();
+            //    Console.WriteLine("ENter details of student " + (i+1));
+            //    students[i].GetDetails();
+            //}
+            //Console.WriteLine("Details of Students");
+            //foreach (Student temp in students)
+            //{
+            //    if(temp != null)
+            //    temp.DisplayDetails();
+            //}
+
+            //Collection
+
+            //ArrayList list = new ArrayList();
+            //Student student1 = new Student();
+            //student1.GetDetails();
+            //list.Add(student1);
+
+            //Student student2 = new Student();
+            //student2.GetDetails();
+            //list.Add(student2);
+
+            //foreach (Student student in list)
+            //{
+            //    student.DisplayDetails();
+            //}
+
+            //list.Add(1);
+            //list.Add("aaa");
+
+            List<Student> students = new List<Student>();
+            Student student1 = new Student();
+            student1.GetDetails();
+            students.Add(student1);
+
+            Student student2 = new Student();
+            student2.GetDetails();
+            students.Add(student2);
+
+            foreach (Student student in students)
+            {
+                student.DisplayDetails();
+            }
+           
+
+
+
+
+        }
+    }
+}
---
+++ OopsDemo/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsDemo
+{
+       class Student
+    {
+        int rn;
+        string name;
+        string batch;
+        int score;
+        public void GetDetails()
+        {
+            Console.WriteLine("enter rn");
+            rn = byte.Parse(Console.ReadLine());
+            Console.WriteLine("enter name");
+            name = Console.ReadLine();
+            Console.WriteLine("enter batch");
+            batch = Console.ReadLine();
+            Console.WriteLine("enter score");
+            score = byte.Parse(Console.ReadLine());
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Rn is {rn}");
+            Console.WriteLine($"Name is {name}");
+            Console.WriteLine($"Batch is {batch}");
+            Console.WriteLine($"Score is {score}");
+        }
+    }
+}
